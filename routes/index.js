3a3ac9d8b9348eeaf6e588466f435d608499dc38
ghina faraj wati <g.farajwati@yahoo.com>
2021-12-24 00:05:16 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,21 @@
 const express = require("express");
 
+
 const hotelRoutes = require("./hotel");
 
 // creates a new router instance.
 const router = express.Router();
 
 router.get("/ping", (req, res) => {
-    const ready = {
-        status: "server is ready",
-    }
+  const ready = {
+    status: "server is ready",
+  };
 
-    res.status(200).send(ready);
-})
+  res.status(200).send(ready);
+});
+
 
 router.use("/hotel", hotelRoutes);
+
 
 module.exports = router;
