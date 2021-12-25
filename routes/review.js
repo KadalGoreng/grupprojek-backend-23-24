923ff0b1e113e1d9e65b26c5d8ba751c3d8ac163
Nazhwa Ameera <68385532+nazhwaameera@users.noreply.github.com>
2021-12-25 11:37:27 +0700

    Update review.js

--- routes/review.js
+++ routes/review.js
@@ -5,7 +5,8 @@ const ReviewController = require("../controllers/review.controller");
 // creates a new router instance.
 const router = express.Router();
 
-router.post("/hotel/addreview", ReviewController.createNewReview);
+router.post("/hotel/:placeId/addreview", ReviewController.createNewReview);
+router.get("/hotel/:placeId/reviews", ReviewController.getReviewByLoc);
 router.post("/wisata/:placeId/addreview", ReviewController.createNewReview);
 router.get("/wisata/:placeId/reviews", ReviewController.getReviewByLoc);
 
